a5c0c54b9088263a096c9c4b7bc2df5563b43106
Shaun Whitely <38450416+shaun-whitely@users.noreply.github.com>
2021-11-28 22:11:07 +1100

    Set up project and add IntroExercises.

---
+++ src/Level01/IntroExercises.hs
@@ -0,0 +1,93 @@
+-- |
+-- Level 1 focuses on basic Scala, including syntax and especially programming with functions.
+-- We will go through a few exercises to familiarise ourselves with writing Scala.
+--
+-- What's a function?
+--
+-- A function takes inputs and returns an output.
+-- It should always return the same output given the same inputs.
+
+module Level01.IntroExercises where
+
+-- |
+-- This function called @add@ takes two Ints and return an Int.
+--
+-- In Haskell, we write the function's type signature on the line above its definition.
+-- If you leave out the type signature, Haskell will infer the type of the function for us the
+-- implementation.
+--
+-- All functions in Haskell are curried functions. You can think of @add@ as a function that takes
+-- an Int and returns a function from Int to Int.
+-- >>> add 1 2
+-- 3
+--
+-- >>> let add1 = add 1
+-- >>> add1 2
+-- 3
+add :: Int -> Int -> Int
+add = undefined
+
+-- |
+-- Reuse the @add@ function and partially apply it for adding 5 to anything.
+--
+-- >>> add5 4
+-- 9
+add5 :: Int -> Int
+add5 = undefined
+
+-- * Parametric Types
+
+-- |
+-- How many ways can you implement this function?
+--
+-- Note: Concrete types start with an uppercase letter. Type parameters start with a lowercase
+-- letter.
+foo :: a -> a
+foo = undefined
+
+-- |
+-- How about this one?
+bar :: Int -> Int
+bar = undefined
+
+-- |
+-- >>> timesTwoIfEven 4
+-- 8
+--
+-- >>> timesTwoIfEven 3
+-- 3
+--
+-- Hint: use the built-in @even@ function.
+timesTwoIfEven :: Int -> Int
+timesTwoIfEven = undefined
+
+-- |
+-- >>> showNumber 100
+-- "The number is 100"
+--
+-- * Hint 1: Use @<>@ to concatenate strings.
+-- * Hint 2: Use @show@ to convert an Int to a String.
+showNumber :: Int -> String
+showNumber = undefined
+
+-- * Tuples
+
+-- |
+-- How can we group together @name@ and @age@ in a pair?
+pair :: String -> Int -> (String, Int)
+pair name age = undefined
+
+-- |
+-- How can we extract the first element of a pair?
+--
+-- * Hint 1: Search Hoogle for "+base (String, Int) -> String". "+base" limits the search to the
+-- standard library.
+-- * Hint 2: Read up on pattern matching:
+-- https://en.wikibooks.org/wiki/Haskell/Pattern_matching#Tuple_constructors
+getName :: (String, Int) -> String
+getName = undefined
+
+-- |
+-- How can we extract the second element of a pair?
+getAge :: (String, Int) -> Int
+getAge = undefined
---
+++ test/Level01/IntroExercisesSpec.hs
@@ -0,0 +1,60 @@
+module Level01.IntroExercisesSpec where
+
+import Test.Hspec
+import Level01.IntroExercises
+
+spec :: Spec
+spec = do
+  describe "add" $ do
+    it "evaluates 1 + 2 as 3" $ do
+      add 1 2 `shouldBe` 3
+
+    it "evaluates 5 + 7 as 12" $ do
+      add 5 7 `shouldBe` 12
+
+  describe "add5" $ do
+    it "evaluates 5 + 4 as 9" $ do
+      add5 4 `shouldBe` 9
+
+    it "evaluates 5 + 10 as 15" $ do
+      add5 10 `shouldBe` 15
+
+  describe "foo" $ do
+    it "can only return the parameter unmodified (aka the 'identity' function)" $ do
+      foo 1 `shouldBe` (1 :: Int)
+
+  describe "bar" $ do
+    it "can be any Int" $ do
+      bar 1 `shouldBe` bar 1
+
+  describe "timesTwoIfEven" $ do
+    it "given 4 returns 8" $ do
+      timesTwoIfEven 4 `shouldBe` 8
+
+    it "given 3 returns 3" $ do
+      timesTwoIfEven 3 `shouldBe` 3
+
+  describe "showNumber" $ do
+    it "constructs the correct string" $ do
+      showNumber 1 `shouldBe` "The number is 1"
+
+  describe "pair" $ do
+    it "tuples Jimmy and 25" $ do
+      pair "Jimmy" 25 `shouldBe` ("Jimmy", 25)
+
+    it "tuples Sammy and 30" $ do
+      pair "Sammy" 30 `shouldBe` ("Sammy", 30)
+
+  describe "getName" $ do
+    it "returns Jimmy's name" $ do
+      getName ("Jimmy", 25) `shouldBe` "Jimmy"
+
+    it "returns Sammy's name" $ do
+      getName ("Sammy", 30) `shouldBe` "Sammy"
+
+  describe "getAge" $ do
+    it "returns Jimmy's age" $ do
+      getAge ("Jimmy", 25) `shouldBe` 25
+
+    it "returns Sammy's age" $ do
+      getAge ("Sammy", 30) `shouldBe` 30
